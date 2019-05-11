a27cd28613be1e739053b10e41fb2b39ff93c706
Abdelrahman Ahmed <AbdelrahmanAbdelfattah@yahoo.com>
2019-05-11 04:10:52 +0200

    design home page add slider,featured, toprated

--- pages/views.py
+++ pages/views.py
@@ -3,9 +3,14 @@ from django.http import HttpResponse
 from projects.models import Project
 # Create your views here.
 def index(request):
-    projects = Project.objects.all()
+    projects = Project.objects.all()[:5]
+    highest_five_rated = Project.objects.all()[:5]
+    latest_five = Project.objects.all()[:5]
+    featured_five = Project.objects.all()[:5]
     #print (projects[0].picture_set.all)
     context = {
-        'projects': projects
+        'highest_five_rated': highest_five_rated ,
+        'latest_five': latest_five,
+        'featured_five': featured_five,
     }
     return render(request, 'pages/index.html', context)
