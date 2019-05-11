b35840a2fbe1c7962d84fe6db418b174a7dbdf57
Mohamed Awad <awadmohamed233@gmail.com>
2019-05-11 11:48:53 +0200

    commit for deploy

--- config/settings.py
+++ config/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/2.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
+import django_heroku
 
 import os
 
@@ -148,3 +149,6 @@ EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'a.abdelfatah.100@gmail.com'
 EMAIL_HOST_PASSWORD = 'bxxpyhkudxpsxllt'
+
+
+django_heroku.settings(locals())
