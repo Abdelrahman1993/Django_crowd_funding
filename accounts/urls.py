5e8c9f868789d302abfbf1d8e60d5b70d39ff840
Mohamed Awad <awadmohamed233@gmail.com>
2019-05-13 03:39:55 +0200

    finish login with email

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,7 +6,7 @@ from django.contrib.auth import views as auth_view
 app_name = 'accounts'
 
 urlpatterns = [
-  path('login/', auth_view.LoginView.as_view(template_name='accounts/login.html'), name='login'),
+  path('login/', views.user_login, name='login'),
   path('logout/', auth_view.LogoutView.as_view(), name='logout'),
   path('signup/', views.Signup.as_view(), name='signup'),
   path('activate/<str:uid>/<str:token>', views.Activate.as_view(), name='activate'),
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,5 @@
 from django.views import View
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect
 from .forms import SignupForm
 from django.contrib.sites.shortcuts import get_current_site
@@ -10,61 +10,85 @@ from django.contrib.auth.forms import PasswordChangeForm
 from django.utils.encoding import force_bytes, force_text
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from .tokens import account_activation_token
+from django.contrib.auth import authenticate, login, logout, password_validation
+from django.contrib.auth.hashers import check_password
 
 
 class Signup(View):
-    def get(self, request):
-        form = SignupForm()
-        return render(request, 'accounts/signup.html', {'form': form})
+  def get(self, request):
+    form = SignupForm()
+    return render(request, 'accounts/signup.html', {'form': form})
 
-    def post(self, request):
-        form = SignupForm(request.POST)
+  def post(self, request):
+    form = SignupForm(request.POST)
 
-        # print(form.cleaned_data['email'])
-        if form.is_valid():
-            # Create an inactive user with no password:
-            user = form.save(commit=False)
-            user.is_active = False
-            user.set_unusable_password()
-            user.save()
+    # print(form.cleaned_data['email'])
+    if form.is_valid():
+      # Create an inactive user with no password:
+      user = form.save(commit=False)
+      user.is_active = False
+      # user.set_unusable_password()
+      user.save()
 
-            # Send an email to the user with the token:
-            # mail_subject = 'Activate your account.'
-            # current_site = get_current_site(request)
-            # uid = urlsafe_base64_encode(force_bytes(user.pk))
-            # token = account_activation_token.make_token(user)
-            # activation_link = "{0}/?uid={1}&token{2}".format(current_site, uid, token)
-            # message = "Hello {0},\n {1}".format(user.first_name + " " + user.last_name, activation_link)
-            # to_email = form.cleaned_data.get('email')
-            # email = EmailMessage(mail_subject, message, to=[to_email])
-            # email.send()
-            return HttpResponse('Please confirm your email address to complete the registration')
-        return redirect('accounts:signup')
+      # Send an email to the user with the token:
+      mail_subject = 'Activate your account.'
+      current_site = get_current_site(request)
+      uid = urlsafe_base64_encode(force_bytes(user.pk))
+      token = account_activation_token.make_token(user)
+      activation_link = "{0}/?uid={1}&token{2}".format(current_site, uid, token)
+      message = "Hello {0},\n {1}".format(user.first_name + " " + user.last_name, activation_link)
+      to_email = form.cleaned_data.get('email')
+      email = EmailMessage(mail_subject, message, to=[to_email])
+      email.send()
+      return HttpResponse('Please confirm your email address to complete the registration')
+    return redirect('accounts:signup', {form})
 
 
 class Activate(View):
-    def get(self, request, uidb64, token):
-        try:
-            uid = force_text(urlsafe_base64_decode(uidb64))
-            user = User.objects.get(pk=uid)
-        except(TypeError, ValueError, OverflowError, User.DoesNotExist):
-            user = None
-        if user is not None and account_activation_token.check_token(user, token):
-            # activate user and login:
-            user.is_active = True
-            user.save()
-            return redirect('accounts:login')
+  def get(self, request, uidb64, token):
+    try:
+      uid = force_text(urlsafe_base64_decode(uidb64))
+      user = User.objects.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+      user = None
+    if user is not None and account_activation_token.check_token(user, token):
+      # activate user and login:
+      user.is_active = True
+      user.save()
+      return redirect('accounts:login')
 
+    else:
+      return HttpResponse('Activation link is invalid!')
+
+  def post(self, request):
+    form = PasswordChangeForm(request.user, request.POST)
+    if form.is_valid():
+      user = form.save()
+      update_session_auth_hash(request, user)  # Important, to update the session with the new password
+      return HttpResponse('Password changed successfully')
+
+
+def user_login(request):
+  if request.method == 'POST':
+    email = request.POST.get('email')
+    password = request.POST.get('password')
+    user = User.objects.get(email__iexact=email)
+    if user and user.check_password(password):
+      if user:
+        if user.is_active:
+          login(request, user)
+          next = request.POST.get('next', '/')
+          return HttpResponseRedirect(next)
         else:
-            return HttpResponse('Activation link is invalid!')
-
-    def post(self, request):
-        form = PasswordChangeForm(request.user, request.POST)
-        if form.is_valid():
-            user = form.save()
-            update_session_auth_hash(request, user) # Important, to update the session with the new password
-            return HttpResponse('Password changed successfully')
-
+          return HttpResponse("Your account was inactive.")
+      else:
+        print("Someone tried to login and failed.")
+        print("They used username: {} and password: {}".format(email, password))
+        return HttpResponse("Invalid login details given")
+    else:
+      return HttpResponse("user not exist")
+  else:
+    return render(request, 'accounts/login.html', {})
 
 # def my_profile(request, pk):
 #   user = User.objects.filter(id=pk)[0]
@@ -103,4 +127,3 @@ class Activate(View):
 #   }
 #
 #   return render(request, 'accounts/profile.html', context)
-
