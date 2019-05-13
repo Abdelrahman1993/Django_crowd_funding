a237445c8e9f4301e70617955943625db2b6dc71
aya77 <ayalaban888@yahoo.com>
2019-05-13 13:55:45 +0200

    comments of project

--- projects/urls.py
+++ projects/urls.py
@@ -3,5 +3,6 @@ from . import views
 app_name='projects'
 urlpatterns = [
     path('', views.index, name="index"),
-    path('<int:_id>', views.project_details, name="project_details")
+    path('<int:project_id>/addComment', views.new_comment, name="addComment"),
+    path('<int:project_id>/', views.project_details, name="project_details")
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,9 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
+from .models import Comment, Project
 
 # Create your views here.
+
+
 def index(request):
     return render(request, 'projects/index.html')
 
-def project_details(request, _id):
-    return render(request, 'projects/project_details.html')
+
+def project_details(request, project_id):
+    project = get_object_or_404(Project, pk=project_id)
+    return render(request, 'projects/project_details.html', {'project': project})
+
+
+def new_comment(request, project_id):
+    project = get_object_or_404(Project, pk=project_id)
+    comment = Comment(
+        user_id=request.POST['user_id'], body=request.POST['body'],
+        project_id=project_id)
+    comment.save()
+    # print("===========================")
+    # print(comment)
+    return render(request, 'projects/project_details.html', {'project': project})
