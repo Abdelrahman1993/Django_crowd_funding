d25ab6601f321ec3e82be165ddf57d39b4e9f6d7
aya77 <ayalaban888@yahoo.com>
2019-05-13 20:10:00 +0200

    add reply on comments

--- projects/views.py
+++ projects/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render, redirect
 from django.urls import reverse
 from django.http import HttpResponse
-from .models import Comment, Project
+from .models import Comment, Project, Reply, Picture
 
 # Create your views here.
 
@@ -13,13 +13,17 @@ def index(request):
 
 def project_details(request, project_id):
     project = get_object_or_404(Project, pk=project_id)
-    return render(request, 'projects/project_details.html', {'project': project})
+    picture = Picture.objects.all().filter(project_id=project_id)
+    print("===================")
+    current_user = request.user
+    print(current_user.id)
+    return render(request, 'projects/project_details.html', {'project': project,'picture':picture[1]})
 
 
 def new_comment(request, project_id):
     project = get_object_or_404(Project, pk=project_id)
     comment = Comment(
-        user_id=request.POST['user_id'], body=request.POST['body'],
+        user_id=request.user, body=request.POST['body'],
         project_id=project_id)
     comment.save()
     return redirect('projects:project_details', project_id=project_id)
@@ -41,4 +45,18 @@ def update_comment(request, comment_id, project_id):
     body = request.POST['body']
     Comment.objects.filter(pk=comment_id).update(body=body)
     project = get_object_or_404(Project, pk=project_id)
+    return redirect('projects:project_details', project_id=project_id)
+
+
+def new_reply(request, comment_id, project_id):
+    project = get_object_or_404(Project, pk=project_id)
+    return render(request, 'projects/new_reply.html', {'comment_id': comment_id, 'project': project})
+
+
+def add_reply(request, comment_id, project_id):
+    comment = get_object_or_404(Comment, pk=comment_id)
+    reply = Reply(
+        user_id=request.user, body=request.POST['body'],
+        comment_id=comment_id)
+    reply.save()
     return redirect('projects:project_details', project_id=project_id)
